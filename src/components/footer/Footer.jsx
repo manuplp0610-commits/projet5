cab584ea7a00127384958e2cacf77112a8e68df6
Manwel Nova <manwel.nova@gmail.com>
2026-08-10 14:31:33 +0200

    update header and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,13 @@
-export default function Footer() {
-  return <div>Footer</div>;
+import "./Footer.scss";
+import logo from "../../assets/images/logo-footer.png";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <img className="footer-logo" src={logo} alt="Logo Kasa" />
+      <p className="footer-text">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 }
+
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,19 @@
+import { NavLink } from "react-router-dom";
+import "../header/header.scss";
+import logo from "../../assets/images/logo.png";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <img className="header-logo" src={logo} alt="Logo Kasa" />
+      <nav className="header-nav">
+        <NavLink to="/" className="header__nav-link">
+          Accueil
+        </NavLink>
+        <NavLink to="/about" className="header__nav-link">
+          À propos
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
