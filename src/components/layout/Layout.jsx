756289c2f6705c9ed8659cb1d41c60fc41088972
Manwel Nova <manwel.nova@gmail.com>
2026-08-16 17:54:07 +0200

    update noFound

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import "../../components/layout/layout.scss";
 
 function Layout() {
   return (
--- src/pages/noFound/NoFound.jsx
+++ src/pages/noFound/NoFound.jsx
@@ -1,3 +1,16 @@
+import "./noFound.scss";
+import { Link } from "react-router-dom";
+
 export default function NoFound() {
-  return <div>NoFound</div>;
+  return (
+    <div className="noFound">
+      <h1 className="noFound-title">404</h1>
+      <p className="noFound-subTitle">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to={"/"} className="noFound-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
